bed5cc6471b2a878377a1b27ac975d11ecba117a
nuno24 <nuno.m1233@gmail.com>
2023-02-12 20:24:31 +0000

    a

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import RegisterScreen from './screens/Register';
-import LoginScreen from './screens/Login';
-import HomeScreen from './screens/Home';
+import Register from './screens/Register';
+import Login from './screens/Login';
+import Home from './screens/Home';
 import ShiftTimes from './screens/ShiftTimes';
 
 const Stack = createNativeStackNavigator();
@@ -11,9 +11,9 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="ShiftTimes" component={ShiftTimes} />
       </Stack.Navigator>
     </NavigationContainer>
